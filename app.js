02e8b9a23bd32600dc9aadedc37f4b14860267da
Chris Markle <markle976@gmail.com>
2012-10-07 21:56:08 -0500

    Connect to mongo

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var application_root = __dirname,
+    express = require("express"),
+    path = require("path"),
+    mongoose = require('mongoose');
+
 var app = express();
 
-app.get('/', function(req, res){
-  res.send('Hello World');
+// Database
+mongoose.connect('mongodb://localhost/ecomm_database');
+
+// Config
+app.configure(function() {
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.get('/api', function(req, res) {
+  res.send('Ecomm API is running');
+});
+
+// Launch server
+
+app.listen(4242);
