a53a01248c4e4f3ec95dc85a75357a770a202ebd
Chris Markle <markle976@gmail.com>
2012-10-11 07:51:11 -0400

    Add REST interface

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-var application_root = __dirname,
-    express = require("express"),
+var express = require("express"),
     path = require("path"),
     mongoose = require('mongoose');
 
 var app = express();
+
+var Product = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    style: { type: String, unique: true },
+    modified: { type: Date, default: Date.now }
+});
+
+var ProductModel = mongoose.model('Product', Product); // new???
 
 // Database
 mongoose.connect('mongodb://localhost/ecomm_database');
@@ -13,14 +21,82 @@ app.configure(function() {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.static(path.join(__dirname, "public")));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
+// routes
 app.get('/api', function(req, res) {
   res.send('Ecomm API is running');
 });
 
-// Launch server
+app.get('/api/products', function (req, res){
+  return ProductModel.find(function (err, products) {
+    if (!err) {
+      return res.send(products);
+    } else {
+      return console.log(err);
+    }
+  });
+});
 
+app.post('/api/products', function (req, res){
+  var product;
+  console.log("POST: ");
+  console.log(req.body);
+  product = new ProductModel({
+    title: req.body.title,
+    description: req.body.description,
+    style: req.body.style,
+  });
+  product.save(function (err) {
+    if (!err) {
+      return console.log("created");
+    } else {
+      return console.log(err);
+    }
+  });
+  return res.send(product);
+});
+
+app.get('/api/products/:id', function (req, res){
+  return ProductModel.findById(req.params.id, function (err, product) {
+    if (!err) {
+      return res.send(product);
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+app.put('/api/products/:id', function (req, res){
+  return ProductModel.findById(req.params.id, function (err, product) {
+    product.title = req.body.title;
+    product.description = req.body.description;
+    product.style = req.body.style;
+    return product.save(function (err) {
+      if (!err) {
+        console.log("updated");
+      } else {
+        console.log(err);
+      }
+      return res.send(product);
+    });
+  });
+});
+
+app.delete('/api/products/:id', function (req, res){
+  return ProductModel.findById(req.params.id, function (err, product) {
+    return product.remove(function (err) {
+      if (!err) {
+        console.log("removed");
+        return res.send('');
+      } else {
+        console.log(err);
+      }
+    });
+  });
+});
+
+// Launch server
 app.listen(4242);
